4d976cd4238211c329c317c4d2389e6fda1c28fd
Tushar Kumar Shahi <tushar.shahi@infracloud.io>
2026-03-11 17:38:58 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+
+    public static String getMessage() {
+        return "Maven Build Composite Action — Test Application";
+    }
+}
